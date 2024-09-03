06f4d317004b600edd49a269d68701d15e9dec22
kudongku <kudongku@naver.com>
2024-09-03 20:52:49 +0900

    feat(global) : Jwt Token의 유효성 검사 기능 구현
    1. HttpRequestHeader에서 tokenValue값 뽑는 기능 구현
    2. token이 expired 되었는지 판단 기능 구햔
    3. 로그로 토큰 값 출력

--- src/main/java/com/soulware/backend/global/util/JwtUtil.java
+++ src/main/java/com/soulware/backend/global/util/JwtUtil.java
@@ -1,22 +1,28 @@
 package com.soulware.backend.global.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+@Slf4j(topic = "JwtUtil")
 @Component
 public class JwtUtil {
 
     @Value("${jwt.secret.key}")
     private String secretKey;
-    private static final Long expiredMs = 1000 * 60 * 60 * 24L;
+    private static final Long expiredMs = 1000 * 30L;
     private static final String BEARER_PREFIX = "Bearer ";
 
     private Key key;
@@ -33,11 +39,36 @@ public class JwtUtil {
 
         return BEARER_PREFIX +
             Jwts.builder()
-            .setClaims(claims)
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
-            .signWith(key, SignatureAlgorithm.HS256)
-            .compact();
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("bearerToken : {}", bearerToken);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+    public boolean isExpired(String token) {
+        try{
+            return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date(System.currentTimeMillis()));
+        }catch (ExpiredJwtException e){
+            return true;
+        }
     }
 
 }
