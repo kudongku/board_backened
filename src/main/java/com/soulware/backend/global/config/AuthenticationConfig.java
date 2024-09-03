01f22f866632ab78e9147bfd4289f133b54709ff
kudongku <kudongku@naver.com>
2024-09-03 20:07:30 +0900

    feat(global) : spring security config 생성
    1. api/users/**만 허용

---
+++ src/main/java/com/soulware/backend/global/config/AuthenticationConfig.java
@@ -0,0 +1,25 @@
+package com.soulware.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class AuthenticationConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/api/users/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+}
