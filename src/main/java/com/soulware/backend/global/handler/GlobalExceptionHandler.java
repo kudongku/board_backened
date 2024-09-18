77a747e931109ebe140238a66fafd163662d5a52
kudongku <kudongku@naver.com>
2024-09-18 23:38:21 +0900

    feat(global) : exception handler 로그 찍는 기능 구현

--- src/main/java/com/soulware/backend/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/soulware/backend/global/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.soulware.backend.global.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j(topic = "GlobalExceptionHandler")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<String> exceptionHandler(Exception ex) {
+        log.error(ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
