acc71aa23d646744f75f8b95d0ab5ab7a5eda9b4
kudongku <kudongku@naver.com>
2024-11-18 14:39:30 +0900

    feat: 환경변수 분리

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,5 @@ FROM openjdk:21-jdk
 # kubernetes 용
 ARG JAR_FILE=./build/libs/backend-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
+ENV SPRING_PROFILES_ACTIVE=prod
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/soulware/backend/BackendApplication.java
+++ src/main/java/com/soulware/backend/BackendApplication.java
@@ -1,15 +1,33 @@
 package com.soulware.backend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j(topic = "BackendApplication")
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
+    private static Environment environment;
+
+    @Autowired
+    public BackendApplication(Environment environment) {
+        BackendApplication.environment = environment;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        BackendApplication app = new BackendApplication(environment);
+        app.contextLoads();
+    }
+
+    public void contextLoads() {
+        log.info("BackendApplication 실행");
+        log.info("profile 값 :: {}", environment.getProperty("spring.profiles.active"));
     }
 
 }
