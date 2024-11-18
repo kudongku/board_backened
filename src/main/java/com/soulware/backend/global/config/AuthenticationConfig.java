9f01de9408e389e530620829080e359b94abc55b
kudongku <kudongku@naver.com>
2024-11-18 14:38:24 +0900

    fix: cors 에러 해결

--- src/main/java/com/soulware/backend/global/config/AuthenticationConfig.java
+++ src/main/java/com/soulware/backend/global/config/AuthenticationConfig.java
@@ -5,10 +5,10 @@ import com.soulware.backend.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +16,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -33,10 +37,7 @@ public class AuthenticationConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(
-                        (sessionManagement) -> sessionManagement.sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS)
-                )
+                .cors(withDefaults())
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
                                 .requestMatchers(
@@ -50,7 +51,8 @@ public class AuthenticationConfig {
                                         "/configuration/security"// Swagger 보안 설정
                                 ).permitAll()
                                 .requestMatchers("/health").permitAll()
-                                .requestMatchers("/api/users/*").permitAll()
+                                .requestMatchers("/api/users/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
@@ -59,13 +61,23 @@ public class AuthenticationConfig {
 
     @Bean
     public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(
+                List.of("Authorization", "content-type", "x-auth-token", "Content-Disposition")
+        );
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(List.of(
+                "Access-Control-Allow-Headers", "Authorization", "x-xsrf-token",
+                "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Content-Disposition"
+        ));
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // 모든 도메인 허용
-        config.addAllowedHeader("*");        // 모든 헤더 허용
-        config.addAllowedMethod("*");        // 모든 메서드 허용
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/**", configuration);
+
         return new CorsFilter(source);
     }
 
